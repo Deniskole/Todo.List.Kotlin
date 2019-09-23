f1d2c937646e9fba1f7fbb7cbcbb5fb2a87a3aae
deniskole <deniskole1@gmail.com>
2019-09-23 17:23:16 +0300

    Todo List: Implements -AlertDialog -RecyclerView -FragmentManager -OnCreateOptionsMenu

---
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.todolist
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        val fragment = FragmentList()
+        fragmentTransaction.add(R.id.containerFrameLayout, fragment, fragment.TAG)
+        fragmentTransaction.commit()
+    }
+}
+
+
+
+
